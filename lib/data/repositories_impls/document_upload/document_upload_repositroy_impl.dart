550bc2526bb782917e2e2a46ca7a6a4076dcf49a
himanshuyadav <himanshuyadav146@gmail.com>
2025-06-14 23:17:53 +0530

    uploading fix

--- lib/data/repositories_impls/document_upload/document_upload_repositroy_impl.dart
+++ lib/data/repositories_impls/document_upload/document_upload_repositroy_impl.dart
@@ -47,37 +47,5 @@ class DocumentRepositoryImpl implements DocumentUploadRepository {
     } catch (e) {
       throw Exception('Failed to upload document: ${e.toString()}');
     }
-    // try {
-    //   final url = Uri.parse(baseUrl + docUpload);
-    //   final file = File(filePath);
-    //
-    //   var request = http.MultipartRequest('POST', url)
-    //     ..headers['Authorization'] =
-    //         'Bearer eyJhbGdvIjoiSFMyNTYiLCJ0eXBlIjoiSldUIiwiZXhwaXJlIjoxNzc5NjY0OTgyfQ==.eyJpc3MiOiJhbGxpbmRpYWl0ci5pbiIsIm1vYmlsZSI6IjkwOTY0NjQ1MzQiLCJ0aW1lIjoxNzQ4MTA4MDMwfQ==.MjhjZWU0NGQyYzU2YTI2MGEwYTYyYjFhZmRlYWI0OWRhM2U2YjI1OThmZjhkYTIwZjZmNTgzNTQyOGM4ZmUzMA=='
-    //     ..fields['fileName'] = fileName
-    //     ..fields['userId'] = userId
-    //     ..fields['itrId'] = itrId
-    //     ..files.add(await http.MultipartFile.fromPath(
-    //       'form16_a',
-    //       file.path,
-    //       filename: fileName,
-    //     ));
-    //
-    //   final response = await request.send();
-    //   final responseData = await response.stream.bytesToString();
-    //
-    //   if (response.statusCode == 200) {
-    //     return {
-    //       'status': 'success',
-    //       'message': 'Document uploaded successfully',
-    //       'fileName': fileName,
-    //       'fileUrl': 'https://allindiaitr.in/uploads/$fileName',
-    //     };
-    //   } else {
-    //     throw Exception('Failed to upload document: ${response.statusCode} - $responseData');
-    //   }
-    // } catch (e) {
-    //   throw Exception('Failed to upload document: ${e.toString()}');
-    // }
   }
 }
